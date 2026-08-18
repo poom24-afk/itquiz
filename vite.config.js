6a4f19433da1091340542671ecb80b7e8694b72d
Phattadon Phongarrom <dolphinpoom24@gmail.com>
2026-08-18 11:02:27 +0700

    Fix asset paths for GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/itquiz/',
+  base: process.env.GITHUB_ACTIONS ? '/itquiz/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
